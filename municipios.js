91692d2a434b905ca2159bf1d7253d181f70e7fc
AlexsanderSoares <alexsandersoares30@gmail.com>
2021-01-21 21:44:36 -0300

    Consumindo api tce

---
+++ municipios.js
@@ -0,0 +1,70 @@
+const municipios = {
+	jardim_do_mulato: {
+		id_ibge: "2205250",
+		id: 1574,
+	},
+	angical_do_piaui: {
+		id_ibge: "2200608",
+		id: 1482,
+	},
+	amarante: {
+		id_ibge: "2200509",
+		id: 1481,
+	},
+	regeneracao: {
+		id_ibge: "2208809",
+		id: 1641,
+	},
+	francisco_ayres: {
+		id_ibge:"2204105",
+		id: 1557,
+	},
+	arraial: {
+		id_ibge:"2201002",
+		id: 1487,
+	},
+	santo_antonio_dos_milagres: {
+		id_ibge:"2209450",
+		id: 1653,
+	},
+	sao_goncalo: {
+		id_ibge:"2209807",
+		id: 1660,
+	},
+	hugo_napoleao: {
+		id_ibge:"2204600",
+		id: 1565,
+	},
+	sao_pedro: {
+		id_ibge:"2210508",
+		id: 1676,
+	},
+	palmeirais: {
+		id_ibge:"2207504",
+		id: 1620,
+	},
+	agricolandia: {
+		id_ibge:"2200103",
+		id: 1474,
+	},
+	agua_branca: {
+		id_ibge:"2200202",
+		id: 1475,
+	},
+	olho_dagua: {
+		id_ibge:"2207108",
+		id: 1615,
+	},
+	barro_duro: {
+		id_ibge:"2201408",
+		id: 1494,
+	},
+	passagem_franca: {
+		id_ibge:"2207751",
+		id: 1624,
+	},
+	lagoinha: {
+		id_ibge:"2205540",
+		id: 1589,
+	}
+}
